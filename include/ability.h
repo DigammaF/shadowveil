d6de6d9166019ca5b7ba6eb938f9e3d0efef10d2
digammaF <digamma.for@protonmail.com>
2025-01-27 15:37:18 +0100

    update

--- include/ability.h
+++ include/ability.h
@@ -4,7 +4,7 @@
 
 #include "champion.h"
 
-#define MAX_ABILITY_DATA 10
+#define MAX_ABILITY_OP_CODES 10
 
 /**
  * 
@@ -16,6 +16,7 @@
  * ADD-EFFECT N ajoute un effet à la cible
  * IF-EFFECT N si la cible n'a pas cet effet, arrêter
  * IF-TYPE N si la cible n'est pas de ce type, arrêter
+ * NOOP pas d'action
  * 
 */
 
@@ -24,10 +25,11 @@
 #define ADD_EFFECT 2
 #define IF_EFFECT 3
 #define IF_TYPE 4
+#define NOOP 5
 
 typedef struct {
     unsigned type;
-    unsigned data[MAX_ABILITY_DATA];
+    unsigned opCodes[MAX_ABILITY_OP_CODES];
     int monoTarget;
 } ability_t;
 
---
+++ src/ability.c
@@ -0,0 +1,34 @@
+
+#include <stdlib.h>
+
+#include "ability.h"
+#include "champion.h"
+
+void applyAbility(champion_t* source, champion_t* destination, ability_t* ability) {
+    unsigned reader = 0;
+    champion_t* target = destination;
+
+    while (reader < MAX_ABILITY_OP_CODES) {
+        unsigned opCode = ability->opCodes[reader];
+        unsigned arg = 0;
+        if (reader + 1 < MAX_ABILITY_OP_CODES) { arg = ability->opCodes[reader + 1]; }
+
+        switch (opCode) {
+            case NOOP:
+                reader++;
+                break;
+
+            case SET_TARGET:
+                if (arg == 0) { target = source; }
+                if (arg == 1) { target = destination; }
+                break;
+
+            case SET_HEALTH:
+                target->health.value += arg;
+                break;
+
+            case ADD_EFFECT:
+                
+        }
+    }
+}
