3997dc7bd9a98cc09c670f9e78736da03dd41ca7
cguillau <clement.guillaumin.dev@gmail.com>
2025-01-27 14:14:15 +0100

    continué character.c + exemples de persos pour test

--- include/character.h
+++ include/character.h
@@ -1,10 +1,21 @@
-#define NB_OF_ATTACKS 3 /** NB_OF_ATTACKS for a character */
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define NB_OF_ATTACKS 3 /** nombre maximum d'attaques par personnage */
+
 
 /** Structure représentant un personnage jouable. */
 typedef struct {
-    int intelligenceStat;
+    int intelligenceStat; /** définit dans quel ordre les personnages agissent */
     int attackStat;
-    int 
-    abilities_t abilities[NB_OF_ATTACKS]; /* attacks */
+    int defenseStat;
+    int magicAttackStat;
+    int magicDefenseStat;
 
+    abilities_t abilities[NB_OF_ATTACKS]; /* pointeurs vers les attaques */
+
+    //ajouts futurs potentiels : arme, armure, niveaux
 } character_t;
+
+
+#endif
