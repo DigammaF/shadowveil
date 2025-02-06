25141c31a571c1165c9d08a249d1d777ef37c02a
DigammaF <digamma.for@protonmail.com>
2025-02-06 23:47:08 +0100

    started working on deal

--- include/game/deal.h
+++ include/game/deal.h
@@ -2,4 +2,20 @@
 #ifndef DEAL_H
 #define DEAL_H
 
+struct champion_t;
+struct item_t;
+struct pawn_t;
+
+typedef struct champion_deal_t {
+	struct champion_t* champion;
+	unsigned price;
+	struct pawn_t* seller;
+} champion_deal_t;
+
+typedef struct item_deal_t {
+	struct item_t* item;
+	unsigned price;
+	struct pawn_t* seller;
+} item_deal_t;
+
 #endif
