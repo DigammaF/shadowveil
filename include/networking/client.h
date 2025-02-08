22e1b260d2f5b2b10f1cc61079aa7245750ffdf8
DigammaF <digamma.for@protonmail.com>
2025-02-08 21:16:59 +0100

    client gui

--- include/networking/client.h
+++ include/networking/client.h
@@ -2,6 +2,9 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <stdbool.h>
+
+#include "hashmap.h"
 #include "lantern.h"
 #include "function_stack.h"
 #include "vector.h"
@@ -10,17 +13,55 @@
 #define MAX_INPUT 300
 
 typedef struct {
+	bool enter; bool tab; bool key_up; bool key_down;
+	char character;
+} keyboard_t;
+
+typedef struct {
+	function_t update; // (widget_context_t*)
+	function_t render; // (widget_context_t*)
+	void* properties; // possède les valeurs
+	unsigned key;
+} widget_t;
+
+typedef struct {
+	hashmap_t widgets; // possède les valeurs, widget_t*
+	keyboard_t keyboard;
+} gui_t;
+
+typedef struct {
 	unsigned running;
 	socket_t socket;
 	function_stack_t contextHandlers;
 	fd_set ioState;
+	gui_t* gui;
 } client_t;
+
+typedef struct {
+	client_t* client;
+	widget_t* widget;
+} widget_context_t;
 
 void initClient(client_t* client);
 void dropClient(client_t* client);
 
+void initWidget(widget_t* widget);
+void dropWidget(widget_t* widget);
+
+void initGUI(gui_t* gui);
+void dropGUI(gui_t* gui);
+
 int mainClient(int argc, const char* argv[]);
 int mainGUIClient(int argc, const char* argv[]);
+
+void addWidgetToGUI(gui_t* gui, widget_t* widget);
+void removeWidgetFromGUI(gui_t* gui, widget_t* widget);
+
+typedef struct {
+	client_t* client;
+	char** args;
+	unsigned argCount;
+} server_request_context_t;
 
 void* initialClientHandler(void* _);
 void* loginClienthandler(void* _);
@@ -32,5 +73,13 @@ void* buyItemClientHandler(void* _);
 void* buyChampionClienthandler(void* _);
 void* sellItemClienthandler(void* _);
 void* sellChampionClientHandler(void* _);
+
+typedef struct {
+	WINDOW* window;
+	unsigned select;
+	char** options;
+	unsigned optionCount;
+	bool validated;
+} menu_properties_t;
 
 #endif
--- src/networking/client.c
+++ src/networking/client.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,6 +6,7 @@
 #include <string.h>
 #include <string.h>
 #include <ncurses.h>
+#include <stdbool.h>
 
 #include "client.h"
 #include "lantern.h"
@@ -17,11 +19,35 @@
 #define CHECKM(status, message) { if ((status) == NULL) { perror(message); exit(EXIT_FAILURE); } }
 
 void initClient(client_t* client) {
+	client->gui = NULL;
 	initFunctionStack(&client->contextHandlers);
 }
 
 void dropClient(client_t* client) {
 	dropFunctionStack(&client->contextHandlers);
+}
+
+void initWidget(widget_t* widget) {
+	widget->update = noHandler;
+	widget->render = noHandler;
+	widget->properties = NULL;
+}
+
+void dropWidget(widget_t* widget) { UNUSED(widget); }
+
+void initGUI(gui_t* gui) {
+	initHashmap(&gui->widgets);
+}
+
+void dropGUI(gui_t* gui) {
+	// TODO free
+	dropHashmap(&gui->widgets);
+}
+
+void resetKeyboard(keyboard_t* keyboard) {
+	keyboard->enter = false;
+	keyboard->tab = false;
+	keyboard->character = '\0';
 }
 
 void setupClientFileDescriptorSet(client_t* client, fd_set* fileDescriptorSet, int* maxFileDescriptor) {
@@ -98,171 +124,214 @@ int mainClient(int argc, const char* argv[]) {
 	return 0;
 }
 
-void displayMenu(WINDOW* window, unsigned select, char** options, unsigned optionCount) {
-    int x = 2, y = 1;
-    box(window, 0, 0);
+// ------------------------------------------------------------------------------------------------------
 
-    for (unsigned n = 0; n < optionCount; n++) {
-        if (n == select) { wattron(window, A_REVERSE); }
-        mvwprintw(window, y + n, x, "%s", options[n]);
-        wattroff(window, A_REVERSE);
-    }
+void addWidgetToGUI(gui_t* gui, widget_t* widget) {
+	widget->key = hashmapLocateUnusedKey(&gui->widgets);
+	hashmapSet(&gui->widgets, widget->key, widget);
 }
 
-void* initialClientHandler(void* _) {
-	client_t* client = _;
+void removeWidgetFromGUI(gui_t* gui, widget_t* widget) {
+	hashmapSet(&gui->widgets, widget->key, NULL);
+}
 
-	char* options[] = { "(I)dentification", "(E)nregistrement" };
-	unsigned optionCount = 2;
-	unsigned select = 0;
+// ------------------------------------------------------------------------------------------------------
 
-	int startx = (COLS - 20) / 2;
-    int starty = (LINES - 2) / 2;
-	WINDOW* window = newwin(4, 20, starty, startx);
-    keypad(window, TRUE);
+void* updateMenu(void* _) {
+	widget_context_t* context = (widget_context_t*)_;
+	widget_t* widget = context->widget;
+	client_t* client = context->client;
+	gui_t* gui = client->gui;
+	keyboard_t* keyboard = &gui->keyboard;
+	menu_properties_t* properties = widget->properties;
 
-	while (1) {
-		displayMenu(window, select, options, optionCount);
-		wrefresh(window);
-		awaitInput(client);
+	if (properties->validated) { return NULL; }
 
-		if (FD_ISSET(fileno(stdin), &client->ioState)) {
-			int character = wgetch(window);
-			if (character == KEY_UP) {
-				if (select > 0) { select--; }
-			}
+	if (keyboard->key_up) {
+		if (properties->select > 0) { properties->select--; }
+	}
 
-			if (character == KEY_DOWN) {
-				if (select < optionCount - 1) { select++; }
-			}
+	if (keyboard->key_down) {
+		if (properties->select < properties->optionCount - 1) { properties->select++; }
+	}
 
-			if (character == '\n') {
-				if (select == 0) {
-					pushFunction(&client->contextHandlers, loginClienthandler);
-					wclear(window); wrefresh(window);
-					delwin(window);
-					return NULL;
-				}
-			}
-		}
+	if (keyboard->enter) {
+		properties->validated = true;
 	}
 
 	return NULL;
 }
 
-void* loginClienthandler(void* _) {
-	client_t* client = (client_t*)_;
+void* renderMenu(void* _) {
+	widget_context_t* context = (widget_context_t*)_;
+	widget_t* widget = context->widget;
+	menu_properties_t* properties = widget->properties;
 
-	unsigned width = 60;
-	int startx = (COLS - width) / 2;
-	int starty = (LINES - 10) / 2;
-	WINDOW* window = newwin(4, width, starty, startx);
-	WINDOW* output = newwin(4, width, starty + 5, startx);
-	unsigned outputY = 2;
+	int x = 2, y = 1;
+    box(properties->window, 0, 0);
 
-	char login[MAX_INPUT] = { 0 };
-	char password[MAX_INPUT] = { 0 };
-	unsigned loginHead = 0;
-	unsigned passwordHead = 0;
-	unsigned editionMode = 1;
+    for (unsigned n = 0; n < properties->optionCount; n++) {
+        if (n == properties->select) { wattron(properties->window, A_REVERSE); }
+        mvwprintw(properties->window, y + n, x, "%s", properties->options[n]);
+		fprintf(stderr, "drawing\n");
+        wattroff(properties->window, A_REVERSE);
+    }
 
-	box(window, 0, 0);
-    mvwprintw(window, 1, 2, ">Nom d'utilisateur: ");
-	unsigned loginTextWidth = 21;
-	wmove(window, 1, loginTextWidth);
-	mvwprintw(window, 2, 2, " Mot de passe: ");
-	unsigned passwordTextWidth = 16;
-	wrefresh(window);
-	
-	while (1) {
-		awaitInput(client);
+	return NULL;
+}
 
-		while (FD_ISSET(client->socket.fileDescriptor, &client->ioState)) {
-			char data[1024];
-			unsigned argCount;
-			int byteCount = recvData(&client->socket, data, 1024);
-			char** args = splitString(data, &argCount);
+void makeMenu(
+	widget_t* widget,
+	unsigned startx, unsigned starty, unsigned width,
+	char** options, unsigned optionCount, unsigned initialSelect
+) {
+	widget->update = updateMenu;
+	widget->render = renderMenu;
+	menu_properties_t* properties = malloc(sizeof(menu_properties_t));
+	properties->window = newwin(optionCount + 2, width, starty, startx);
+	keypad(properties->window, TRUE);
+	properties->select = initialSelect;
+	properties->options = options;
+	properties->optionCount = optionCount;
+	properties->validated = false;
+	widget->properties = properties;
+}
 
-			if (byteCount == 0) {
-				client->running = 0;
-				return NULL;
-			}
+// -------------------------------------------------------------------------------------------------------
 
-			if (argCount == 2) {
-				if (strcmp(args[0], "SET-CONTEXT") == 0) {
-					context_t context = (context_t)atoi(args[1]);
+void setupInitial(client_t* client) {
+	gui_t* gui = client->gui;
+	char** options = malloc(2*sizeof(char*));
+	CHECKM(options, "malloc options");
+	options[0] = "Identification";
+	options[1] = "Enregistrement";
+	widget_t* menu = malloc(sizeof(widget_t));
+	initWidget(menu);
+	makeMenu(
+		menu,
+		(COLS - 20) / 2, (LINES - 2) / 2, 20,
+		options, 2, 0
+	);
+	addWidgetToGUI(gui, menu);
+}
 
-					if (context == GAMEWORLD) {
-						pushFunction(&client->contextHandlers, loginClienthandler);
-						clear(); refresh();
-						return NULL;
-					}
-				}
-			}
+// -------------------------------------------------------------------------------------------------------
 
-			if (argCount > 2) {
-				if (strcmp(args[0], "OUTPUT") == 0) {
-					char* text = joinString(&args[1], " ");
-					mvwprintw(output, 1, outputY, "serveur: %s", text);
-					outputY++;
-					freeJoin(text);
-				}
+void* initialClientHandler(void* _) {
+	return NULL;
+}
 
-				if (strcmp(args[0], "ERROR") == 0) {
-					char* text = joinString(&args[1], " ");
-					mvwprintw(output, 1, outputY, "erreur: %s", text);
-					outputY++;
-					freeJoin(text);
-				}
-			}
-		}
+void handleServerResponse(client_t* client, WINDOW* output, unsigned* outputY) {
+	char data[1024];
+	unsigned argCount;
+	int byteCount = recvData(&client->socket, data, 1024);
+	char** args = splitString(data, &argCount);
 
-		while (FD_ISSET(fileno(stdin), &client->ioState)) {
-			int character = wgetch(window);
+	if (byteCount == 0) {
+		client->running = 0;
+		return;
+	}
 
-			if (character == '\n') {
-				login[loginHead] = '\0';
-				password[passwordHead] = '\0';
-				mvwprintw(output, 1, outputY, "Identification avec %s / %s ...", login, password);
-				outputY++;
-				wrefresh(output);
-				char message[1024];
-				sprintf(message, "COMMAND LOGIN %s %s", login, password);
-				sendData(&client->socket, message);
-			} else if (character == 127) {
-				if ((editionMode == 1 && loginHead > 0) || (editionMode == 2 && passwordHead > 0)) {
-					unsigned x = editionMode == 1 ? loginTextWidth + loginHead : passwordTextWidth + passwordHead;
-					mvwprintw(window, editionMode, x, " ");
-					wmove(window, editionMode, x);
-					if (editionMode == 1) { loginHead--; }
-					if (editionMode == 2) { passwordHead--; }
-				}
-			} else if (
-				((editionMode == 1 && loginHead < MAX_INPUT) || (editionMode == 2 && passwordHead < MAX_INPUT))
-				&& character >= 32 && character <= 126
-			) {
-				unsigned x;
-
-				if (editionMode == 1) {
-					login[loginHead] = character;
-					loginHead++;
-					x = loginTextWidth + loginHead;
-				} else {
-					password[passwordHead] = character;
-					passwordHead++;
-					x = passwordTextWidth + passwordHead;
-				}
-				
-				mvwprintw(window, editionMode, x, "%c", character);
-			} else if (character == 9) {
-				mvwprintw(window, editionMode, 2, " ");
-				editionMode = (editionMode == 1) ? 2 : 1;
-				mvwprintw(window, editionMode, 2, ">");
-			}
+	if (argCount == 2 && strcmp(args[0], "SET-CONTEXT") == 0) {
+		context_t context = (context_t)atoi(args[1]);
+		if (context == GAMEWORLD) {
+			pushFunction(&client->contextHandlers, loginClienthandler);
+			clear(); refresh();
+			return;
 		}
 	}
 
-	wrefresh(window);
+	if (argCount > 2) {
+		char* text = joinString(&args[1], " ");
+		if (strcmp(args[0], "OUTPUT") == 0) {
+			mvwprintw(output, 1, *outputY, "serveur: %s", text);
+		} else if (strcmp(args[0], "ERROR") == 0) {
+			mvwprintw(output, 1, *outputY, "erreur: %s", text);
+		}
+		(*outputY)++;
+		freeJoin(text);
+	}
+}
+
+void handleUserInput(client_t* client, WINDOW* window, WINDOW* output, unsigned* outputY, char* login, unsigned* loginHead, char* password, unsigned* passwordHead, unsigned* editionMode) {
+	int character = wgetch(window);
+
+	if (character == '\n') {
+		login[*loginHead] = '\0';
+		password[*passwordHead] = '\0';
+		mvwprintw(output, 1, *outputY, "Identification avec %s / %s ...", login, password);
+		(*outputY)++;
+		wrefresh(output);
+		char message[1024];
+		sprintf(message, "COMMAND LOGIN %s %s", login, password);
+		sendData(&client->socket, message);
+	} else if (character == 127) {
+		if ((*editionMode == 1 && *loginHead > 0) || (*editionMode == 2 && *passwordHead > 0)) {
+			unsigned x = *editionMode == 1 ? 21 + *loginHead : 16 + *passwordHead;
+			mvwprintw(window, *editionMode, x, " ");
+			wmove(window, *editionMode, x);
+			if (*editionMode == 1) { (*loginHead)--; }
+			if (*editionMode == 2) { (*passwordHead)--; }
+		}
+	} else if (
+		((*editionMode == 1 && *loginHead < MAX_INPUT) || (*editionMode == 2 && *passwordHead < MAX_INPUT))
+		&& character >= 32 && character <= 126
+	) {
+		unsigned x;
+		if (*editionMode == 1) {
+			login[*loginHead] = character;
+			(*loginHead)++;
+			x = 21 + *loginHead;
+		} else {
+			password[*passwordHead] = character;
+			(*passwordHead)++;
+			x = 16 + *passwordHead;
+		}
+		mvwprintw(window, *editionMode, x, "%c", character);
+	} else if (character == 9) {
+		mvwprintw(window, *editionMode, 2, " ");
+		*editionMode = (*editionMode == 1) ? 2 : 1;
+		mvwprintw(window, *editionMode, 2, ">");
+	}
+}
+
+void* loginClienthandler(void* _) {
+	// client_t* client = (client_t*)_;
+
+	// unsigned width = 60;
+	// int startx = (COLS - width) / 2;
+	// int starty = (LINES - 10) / 2;
+	// WINDOW* window = newwin(4, width, starty, startx);
+	// WINDOW* output = newwin(4, width, starty + 5, startx);
+	// unsigned outputY = 2;
+
+	// char login[MAX_INPUT] = { 0 };
+	// char password[MAX_INPUT] = { 0 };
+	// unsigned loginHead = 0;
+	// unsigned passwordHead = 0;
+	// unsigned editionMode = 1;
+
+	// box(window, 0, 0);
+    // mvwprintw(window, 1, 2, ">Nom d'utilisateur: ");
+	// unsigned loginTextWidth = 21;
+	// wmove(window, 1, loginTextWidth);
+	// mvwprintw(window, 2, 2, " Mot de passe: ");unsigned passwordTextWidth = 16;
+	// unsigned passwordTextWidth = 16;
+	// wrefresh(window);
+	
+	// while (1) {
+	// 	awaitInput(client);
+
+	// 	while (FD_ISSET(client->socket.fileDescriptor, &client->ioState)) {
+	// 		handleServerResponse(client, output, &outputY);
+	// 	}
+
+	// 	while (FD_ISSET(fileno(stdin), &client->ioState)) {
+	// 		handleUserInput(client, window, output, &outputY, login, &loginHead, password, &passwordHead, &editionMode);
+	// 	}
+	// }
+
+	// wrefresh(window);
 	return NULL;
 }
 
@@ -282,23 +351,78 @@ void* sellItemClienthandler(void* _) { UNUSED(_); return NULL; }
 
 void* sellChampionClientHandler(void* _) { UNUSED(_); return NULL; }
 
+// -------------------------------------------------------------------------------------------------------
+
 int mainGUIClient(int argc, const char* argv[]) {
 	UNUSED(argc); UNUSED(argv);
+	setbuf(stderr, NULL);
 	socket_t clientSocket;
 	connectServer(&clientSocket, "127.0.0.1", SERVER_PORT);
 	client_t client;
 	initClient(&client);
 	client.socket = clientSocket;
 	client.running = 1;
+	gui_t* gui = malloc(sizeof(gui_t));
+	CHECKM(gui, "malloc gui");
+	initGUI(gui);
+	client.gui = gui;
 	pushFunction(&client.contextHandlers, initialClientHandler);
+	setupInitial(&client);
 	// ncurses
 	initscr(); clear(); noecho(); cbreak(); keypad(stdscr, TRUE);
 
 	while (client.running) {
-		function_t handler = peekFunction(&client.contextHandlers);
-		handler((void*)&client);
+		awaitInput(&client);
 
-		if (functionStackEmpty(&client.contextHandlers)) { client.running = 0; }
+		// read server
+		while (FD_ISSET(client.socket.fileDescriptor, &client.ioState)) {
+			server_request_context_t context;
+			context.client = &client;
+			char data[1024];
+			int byteCount = recvData(&client.socket, data, 1024);
+			unsigned argCount;
+			char** args = splitString(data, &argCount);
+
+			if (byteCount == 0) { client.running = 0; return 0; }
+
+			function_t handler = peekFunction(&client.contextHandlers);
+			handler((void*)&context);
+
+			if (functionStackEmpty(&client.contextHandlers)) { client.running = 0; }
+		}
+		
+		// read keyboard
+		while (FD_ISSET(fileno(stdin), &client.ioState)) {
+			keyboard_t* keyboard = &gui->keyboard;
+			resetKeyboard(keyboard);
+			int character = getch();
+
+			if (character == '\n') { keyboard->enter = true; }
+			if (character == 9) { keyboard->tab = true; }
+			if (character == KEY_UP) { keyboard->key_up = true; }
+			if (character == KEY_DOWN) { keyboard->key_down = true; }
+			if (character >= 32 && character <= 126) { keyboard->character = (char)character; }
+
+			for (unsigned n = 0; n < gui->widgets.capacity; n++) {
+				widget_t* widget = gui->widgets.elements[n];
+				if (widget == NULL) { continue; }
+				widget_context_t context = { .widget = widget, .client = &client };
+				widget->update((void*)&context);
+			}
+		}
+
+		// rendering
+		clear();
+
+		for (unsigned n = 0; n < gui->widgets.capacity; n++) {
+			widget_t* widget = gui->widgets.elements[n];
+			if (widget == NULL) { continue; }
+			widget_context_t context = { .widget = widget, .client = &client };
+			widget->render((void*)&context);
+		}
+
+		fprintf(stderr, "refreshing\n");
+		refresh();
 	}
 
 	endwin();
